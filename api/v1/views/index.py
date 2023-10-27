06c35fff30484a282c083c3f2a40e7980d7b960c
elyse502 <elyseniyibizi502@gmail.com>
2023-10-27 02:18:42 -0700

    Update solutions for Task 4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,7 +16,7 @@ def status():
 
 @app_views.route('/stats', strict_slashes=False)
 def stats():
-    """Retrieve count of objects in storage
+    """Retrieve count of objects in Storage
     """
     from models.amenity import Amenity
     from models.city import City

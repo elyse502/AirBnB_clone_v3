b87792cadc59c9c1488d5f2a1bbab95318be8e95
NIYIBIZI Elysée <125453474+elyse502@users.noreply.github.com>
2023-10-27 14:37:53 +0200

    Update solutions for Task 4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3
 """Define routes for blueprint
-This module contains endpoint(route) status
 """
-from models import storage
-from flask import Flask
+
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
+    """Return status of application
     """
-    Returns a JSON status
-    """
-    return jsonify({"status": "OK"})
+    return jsonify({'status': 'OK'})
 
 
 @app_views.route('/stats', strict_slashes=False)
-def count():
+def stats():
+    """Retrieve count of objects in storage
     """
-    Retrieves the number of each objects by type
-    """
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+
+    classes = {"amenities": Amenity, "cities": City,
+               "places": Place, "reviews": Review,
+               "states": State, "users": User}
+    json_dict = {}
+
+    for name, cls in classes.items():
+        json_dict.update({name: storage.count(cls)})
+
+    return jsonify(json_dict)
